950473013646b4be30327486385b635c6972edd0
tauhid537 <ariyankjan7777@gmail.com>
2026-07-14 22:58:19 +0600

    Add files via upload

---
+++ Script/events/antiout.js
@@ -0,0 +1,27 @@
+module.exports.config = {
+ name: "antiout",
+ eventType: ["log:unsubscribe"],
+ version: "1.0.0",
+ credits: "SHAHADAT SAHU",
+ description: "Listen events"
+};
+
+module.exports.run = async({ event, api, Threads, Users }) => {
+ let data = (await Threads.getData(event.threadID)).data || {};
+ if (data.antiout == false) return;
+ if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+ const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "Koi Ase Pichware Mai Lath Marta Hai?";
+ if (type == "self-separation") {
+  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+   if (error) {
+    api.sendMessage(`সরি বস, ${name} কে আবার এড করতে পারলাম না। 
+সম্ভবত উনি বটকে ব্লক করেছে অথবা তার প্রাইভেসি সেটিংসের কারণে এড করা যায় না। 
+\n──────꯭─⃝‌‌𝐌𝐢𝐦𝐢 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭─────`, event.threadID)
+   } else api.sendMessage(`শোন, ${name}, এই গ্রুপ হইলো গ্যাং!
+এখান থেকে যাইতে হলে এডমিনের পারমিশন লাগে!
+তুই পারমিশন ছাড়া লিভ নিছোস – তোকে আবার মাফিয়া স্টাইলে এড দিলাম।
+\n──────꯭─⃝‌‌𝐌𝐢𝐦𝐢 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭─────`, event.threadID);
+  })
+ }
+}
---
+++ Script/events/god.js
@@ -0,0 +1,64 @@
+module.exports.config = {
+	name: "god",
+	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
+	version: "1.0.0",
+	credits: "SHAHADAT SAHU",
+	description: "Record bot activity notifications!",
+	envConfig: {
+		enable: true
+	}
+};
+
+module.exports.run = async function({ api, event, Threads }) {
+	const logger = require("../../utils/log");
+	if (!global.configModule[this.config.name].enable) return;
+	
+	let formReport = "=== ─꯭─⃝‌‌𝐌𝐢𝐦𝐢 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
+					"\n\n» Thread ID: " + event.threadID +
+					"\n» Action: {task}" +
+					"\n» Action created by userID: " + event.author +
+					"\n» " + Date.now() + " «";
+	
+	let task = "";
+	
+	switch (event.logMessageType) {
+		case "log:thread-name": {
+			const oldName = (await Threads.getData(event.threadID)).name || "Name does not exist";
+			const newName = event.logMessageData.name || "Name does not exist";
+			task = "User changed group name from: '" + oldName + "' to '" + newName + "'";
+			await Threads.setData(event.threadID, { name: newName });
+			break;
+		}
+		case "log:subscribe": {
+			if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+				task = "The user added the bot to a new group!";
+			}
+			break;
+		}
+		case "log:unsubscribe": {
+			if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) {
+				task = "The user kicked the bot out of the group!";
+			}
+			break;
+		}
+		default: 
+			break;
+	}
+
+	if (task.length === 0) return;
+
+	formReport = formReport.replace(/\{task}/g, task);
+
+	const receivers = [
+		"100084234082357",   // Replace youR UID
+		"100084234082357"   //  Replace youR Group UID
+	];
+
+	for (const id of receivers) {
+		try {
+			await api.sendMessage(formReport, id);
+		} catch (error) {
+			logger(formReport, "[ Logging Event ]");
+		}
+	}
+};
